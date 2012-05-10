3d4a5ddfadd1541ba1e955a36089884b34fca2f2
Aaron Hicks <hicksa@landcareresearch.co.nz>
2012-05-11 11:09:38 +1200

    create a link to a versionless directory

--- manifests/core/download.pp
+++ manifests/core/download.pp
@@ -1,20 +1,34 @@
 # NeSI manifest to retireve and unpack the Ganglia Core archive
 
-class ganglia::core::download(
-  $install_path   = '/opt'
-){
+class ganglia::core::download{
 
   include ganglia::parameters
 
-  file{$install_path:
+  file{$ganglia::parameters::install_root:
     ensure => directory,
   }
 
   exec{'get_core':
-    cwd   => $install_path,
-    path  => ['/usr/bin','/bin'],
-    user  => root,
+    cwd     => $ganglia::parameters::install_root,
+    path    => ['/usr/bin','/bin'],
+    user    => root,
     command => "wget -O - ${ganglia::parameters::core_source_url}|tar xzv",
-    require => File[$install_path],
+    creates => $ganglia::parameters::core_source_dir,
+    require => File[$ganglia::parameters::install_root],
+  }
+
+  file{$ganglia::parameters::core_source_dir:
+    ensure  => directory,
+    recurse => true,
+    owner   => root,
+    group   => root,
+    require => Exec['get_core'],
+  }
+
+  file{$ganglia::parameters::core_dir:
+    ensure  => link,
+    path    => $ganglia::parameters::core_dir,
+    target  => $ganglia::parameters::core_source_dir,
+    require => File[$ganglia::parameters::core_source_dir]
   }
 }
