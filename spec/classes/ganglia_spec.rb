624a42b711523bc45868079fbaf0100718473d24
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-08-15 13:21:44 +1200

    Update spec test to handle rrd conditions.

--- spec/classes/ganglia_spec.rb
+++ spec/classes/ganglia_spec.rb
@@ -1,20 +1,64 @@
 require 'spec_helper'
 
 describe 'ganglia', :type => :class do
-  context "on a Debian OS" do
+  context 'on a Debian OS' do
     let :facts do
       {
         :osfamily               => 'Debian',
       }
     end
     it {should include_class('ganglia::params')}
+    it {should_not include_class('rrd')}
+    it {should_not contain_package('librrd4')}
+    describe 'with manage_rrd => package' do
+      let :params do
+        { :manage_rrd => 'package'}
+      end
+      it {should contain_package('librrd4')}
+    end
+    describe 'with manage_rrd => module' do
+      let :params do
+        { :manage_rrd => 'module'}
+      end
+      # it {should include_class('rrd')}
+      it {should_not contain_package('librrd4')}
+    end
+    describe 'with manage_rrd => require' do
+      let :params do
+        { :manage_rrd => 'require'}
+      end
+      # it {should include_class('rrd')}
+      it {should_not contain_package('librrd4')}
+    end
   end
-  context "on a RedHat OS" do
+  context 'on a RedHat OS' do
     let :facts do
       {
         :osfamily               => 'RedHat',
       }
     end
     it {should include_class('ganglia::params')}
+    it {should_not include_class('rrd')}
+    it {should_not contain_package('rrdtool')}
+    describe 'with manage_rrd => package' do
+      let :params do
+        { :manage_rrd => 'package'}
+      end
+      it {should contain_package('rrdtool')}
+    end
+    describe 'with manage_rrd => module' do
+      let :params do
+        { :manage_rrd => 'module'}
+      end
+      # it {should include_class('rrd')}
+      it {should_not contain_package('rrdtool')}
+    end
+    describe 'with manage_rrd => require' do
+      let :params do
+        { :manage_rrd => 'require'}
+      end
+      # it {should include_class('rrd')}
+      it {should_not contain_package('rrdtool')}
+    end
   end
 end
