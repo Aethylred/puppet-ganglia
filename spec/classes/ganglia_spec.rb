8b7ea1e864a50db882c546decd5112fdd1f3d951
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-08-15 15:55:52 +1200

    Trying to create a fake class for the external rrd class for spec testing

--- spec/classes/ganglia_spec.rb
+++ spec/classes/ganglia_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe 'ganglia', :type => :class do
+  before(:each) do
+    sub_const('rrd', double('Test double for rrd'))
+  end
   context 'on a Debian OS' do
     let :facts do
       {
@@ -14,20 +17,21 @@ describe 'ganglia', :type => :class do
       let :params do
         { :manage_rrd => 'package'}
       end
+      it {should_not include_class('rrd')}
       it {should contain_package('librrd4')}
     end
     describe 'with manage_rrd => module' do
       let :params do
         { :manage_rrd => 'module'}
       end
-      # it {should include_class('rrd')}
+      it {should include_class('rrd')}
       it {should_not contain_package('librrd4')}
     end
     describe 'with manage_rrd => require' do
       let :params do
         { :manage_rrd => 'require'}
       end
-      # it {should include_class('rrd')}
+      it {should include_class('rrd')}
       it {should_not contain_package('librrd4')}
     end
   end
@@ -44,20 +48,21 @@ describe 'ganglia', :type => :class do
       let :params do
         { :manage_rrd => 'package'}
       end
+      it {should_not include_class('rrd')}
       it {should contain_package('rrdtool')}
     end
     describe 'with manage_rrd => module' do
       let :params do
         { :manage_rrd => 'module'}
       end
-      # it {should include_class('rrd')}
+      it {should include_class('rrd')}
       it {should_not contain_package('rrdtool')}
     end
     describe 'with manage_rrd => require' do
       let :params do
         { :manage_rrd => 'require'}
       end
-      # it {should include_class('rrd')}
+      it {should include_class('rrd')}
       it {should_not contain_package('rrdtool')}
     end
   end
